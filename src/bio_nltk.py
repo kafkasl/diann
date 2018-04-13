da4d189cbf1e8240c6d12d1ed676ab316c0851ab
Pol.Alvarez@BSC <pol.avms@gmail.com>
2018-04-13 09:32:55 +0200

    created class for tagger and added crf

--- src/bio_nltk.py
+++ src/bio_nltk.py
@@ -1,24 +1,16 @@
 from glob import glob
-from sklearn.neural_network import MLPClassifier
-from nltk.stem.snowball import SnowballStemmer
-from collections import Iterable
-from nltk.tag import ClassifierBasedTagger
-from nltk.chunk import ChunkParserI
-from xml.parsers.expat import ExpatError
+
 from collections import defaultdict
 from lxml import etree
-import numpy as np
 from lxml.etree import XMLSyntaxError
 from multiprocessing import Pool
 from nerc_evaluator import nerc_evaluation
+
+import numpy as np
+
 import re
-import pickle
-import xmltodict
 import argparse
 import nltk
-import json
-import string
-import sys
 
 
 def remove_tags(raw):
@@ -50,84 +42,6 @@ def to_conll_iob(annotated_sentence):
                 ner = "B-" + ner
         proper_iob_tokens.append((tag, word, ner))
     return proper_iob_tokens
-
-
-def features(tokens, index, history):
-    """
-    `tokens`  = a POS-tagged sentence [(w1, t1), ...]
-    `index`   = the index of the token we want to extract features for
-    `history` = the previous predicted IOB tags
-    """
-
-    # init the stemmer
-    stemmer = SnowballStemmer('english')
-
-    # Pad the sequence with placeholders
-    tokens = [('[START2]', '[START2]'), ('[START1]', '[START1]')] + list(tokens) + [('[END1]', '[END1]'),
-                                                                                    ('[END2]', '[END2]')]
-    history = ['[START2]', '[START1]'] + list(history)
-
-    # shift the index with 2, to accommodate the padding
-    index += 2
-
-    word, pos = tokens[index]
-    prevword, prevpos = tokens[index - 1]
-    prevprevword, prevprevpos = tokens[index - 2]
-    nextword, nextpos = tokens[index + 1]
-    nextnextword, nextnextpos = tokens[index + 2]
-    previob = history[index - 1]
-    contains_dash = '-' in word
-    contains_dot = '.' in word
-    allascii = all([True for c in word if c in string.ascii_lowercase])
-
-    allcaps = word == word.capitalize()
-    capitalized = word[0] in string.ascii_uppercase
-
-    prevallcaps = prevword == prevword.capitalize()
-    prevcapitalized = prevword[0] in string.ascii_uppercase
-
-    nextallcaps = prevword == prevword.capitalize()
-    nextcapitalized = prevword[0] in string.ascii_uppercase
-
-    return {
-        'word': word,
-        'lemma': stemmer.stem(word),
-        'pos': pos,
-        'all-ascii': allascii,
-
-        'next-word': nextword,
-        'next-lemma': stemmer.stem(nextword),
-        'next-pos': nextpos,
-
-        'next-next-word': nextnextword,
-        'nextnextpos': nextnextpos,
-
-        'prev-word': prevword,
-        'prev-lemma': stemmer.stem(prevword),
-        'prev-pos': prevpos,
-
-        'prev-prev-word': prevprevword,
-        'prev-prev-pos': prevprevpos,
-
-        'prev-iob': previob,
-
-        'contains-dash': contains_dash,
-        'contains-dot': contains_dot,
-
-        'all-caps': allcaps,
-        'capitalized': capitalized,
-
-        'prev-all-caps': prevallcaps,
-        'prev-capitalized': prevcapitalized,
-
-        'next-all-caps': nextallcaps,
-        'next-capitalized': nextcapitalized,
-
-        # word is in a list consult external disabilities
-        # create own list from the training
-        # be creative
-        # try to remove some which may add noise
-    }
 
 
 def is_tag(text, index):
@@ -243,27 +157,6 @@ def bioDataGenerator(folder, lang, debug):
                 iob_data = process_sentence(sentence)
                 yield iob_data
 
-class NamedEntityChunker(ChunkParserI):
-    def __init__(self, train_sents, use_neg, **kwargs):
-        assert isinstance(train_sents, Iterable)
-        training = []
-
-        self.feature_detector = features
-        self.tagger = ClassifierBasedTagger(
-            train=train_sents,
-            feature_detector=features,
-            **kwargs)
-
-    def parse(self, tagged_sent):
-        chunks = self.tagger.tag(tagged_sent)
-
-        # Transform the result from [((w1, t1), iob1), ...]
-        # to the preferred list of triplets format [(w1, t1, iob1), ...]
-        iob_triplets = [(w, t, c) for ((w, t), c) in chunks]
-
-        # Transform the list of triplets to nltk.Tree format
-        return iob_triplets
-        # return nltk.chunk.conlltags2tree(iob_triplets)
 
 def flatten_to_conll(sentences):
     conll_data = []
